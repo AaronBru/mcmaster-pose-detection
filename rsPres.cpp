ee541b924433bee08eba0b34ac0240a335e86a2f
Aaron Bruinsma <aaron.bruinsma@hotmail.com>
2018-10-31 09:39:09 -0400

    Added marker detection for light blue post it notes. Must fix putText formatting.

--- rsPres.cpp
+++ rsPres.cpp
@@ -41,14 +41,14 @@ static void initRsCam(struct rsConfig &rsCfg)
 }
 
 
-static bool displayUi(int& left, int& up, Mat& image, std::array<float, 3> vector)
+static bool displayUi(int xTarget, int yTarget, Mat& image, std::array<float, 3> vector)
 {
-    circle(image, Point(left, up), 4, Scalar(0, 0, 0), CV_FILLED);
+    circle(image, Point(xTarget, yTarget), 4, Scalar(0, 0, 0), CV_FILLED);
 
     std::string displacement = std::to_string(vector[0]) + ", " + std::to_string(vector[1]) + ", " + std::to_string(vector[2]);
     putText(image, displacement, Point(50, 50),  FONT_HERSHEY_PLAIN, 4, Scalar(0, 0, 0), 0);
 
-    std::string pixel = std::to_string(left) + ", " + std::to_string(up);
+    std::string pixel = std::to_string(xTarget) + ", " + std::to_string(yTarget);
     putText(image, pixel,        Point(50, 100), FONT_HERSHEY_PLAIN, 4, Scalar(0, 0, 0), 0);
 
     namedWindow("Show Image", WINDOW_AUTOSIZE);
@@ -56,23 +56,75 @@ static bool displayUi(int& left, int& up, Mat& image, std::array<float, 3> vecto
 
     char button;
     button = waitKey(1);
+
     if (button == 27) {
         destroyAllWindows();
         return true;
     }
-    else if (button == 'w') {
-        up += 10;
-    }
-    else if (button == 's') {
-        up -= 10;
-    }
-    else if (button == 'a') {
-        left -= 10;
-    }
-    else if (button == 'd') {
-        left += 10;
-    }
+
     return false;
+}
+
+std::array<int, 2> processFrame(Mat& frame)
+{
+    Mat blur;
+    GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
+    
+    Mat hsvFrame;
+    cvtColor(blur, hsvFrame, CV_BGR2HSV);
+
+    Mat rangeRes = Mat::zeros(frame.size(), CV_8UC1);
+    inRange(hsvFrame, Scalar(90, 200, 200), Scalar(100, 255, 255), rangeRes);
+
+    dilate(rangeRes, rangeRes, Mat(), Point(-1, -1), 2);
+
+    std::vector<std::vector<Point>> contours;
+    findContours(rangeRes, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+    std::vector<std::vector<Point>> markers;
+    std::vector<Rect> markerBoxes;
+
+    for (size_t i = 0; i < contours.size(); i++) {
+        Rect boundBox;
+        boundBox = boundingRect(contours[i]);
+        
+        if (boundBox.area() > 400) {
+            markers.push_back(contours[i]);
+            markerBoxes.push_back(boundBox);
+        }
+    }
+
+    Point maxCenter;
+    int maxArea = 0;
+    for (size_t i = 0; i < markers.size(); i++) {
+
+        drawContours(frame, markers, i,  CV_RGB(150, 20, 20), 3);
+        rectangle(frame, markerBoxes[i], CV_RGB(20, 150, 20), 3);
+
+        Point center;
+        center.x = markerBoxes[i].x + markerBoxes[i].width / 2;
+        center.y = markerBoxes[i].y + markerBoxes[i].height / 2;
+
+        int currentArea = markerBoxes[i].area();
+        if (currentArea > maxArea) {
+            maxArea = currentArea;
+            maxCenter.x = center.x;
+            maxCenter.y = center.y;
+        }
+
+        circle(frame, center, 4, CV_RGB(20, 150, 20), CV_FILLED);
+    }
+
+    std::array<int, 2> markerCenter;
+    if (markers.size() == 0) {
+        markerCenter[0] = 100;
+        markerCenter[1] = 100;
+    } 
+    else {
+        markerCenter[0] = maxCenter.x;
+        markerCenter[1] = maxCenter.y;
+    }
+    return markerCenter;
 }
 
 
@@ -81,12 +133,8 @@ int main()
     struct rsConfig rsCfg;
     initRsCam(rsCfg);
 
-
     auto stream = rsCfg.rsProfile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
     auto intrinsics = stream.get_intrinsics();
-
-    int left = rsCfg.colorRes[0] / 2;
-    int up   = rsCfg.colorRes[1] / 2;
 
     float vpixel[2];
     float vPoint[3] = {0,0,0};
@@ -105,17 +153,15 @@ int main()
             continue;
         }
 
-        vpixel[0] = left;
-        vpixel[1] = up;
-        rs2_deproject_pixel_to_point(vPoint, &intrinsics, vpixel, depth.get_distance(left, up));
+        Mat color(Size(rsCfg.colorRes[0], rsCfg.colorRes[1]), CV_8UC3, (void*)other_frame.get_data(), Mat::AUTO_STEP);
+        std::array<int, 2> trackPos = processFrame(color);
+
+        rs2_deproject_pixel_to_point(vPoint, &intrinsics, vpixel, depth.get_distance(trackPos[0], trackPos[1]));
 
         std::array<float, 3> vec = {vPoint[0], vPoint[1], vPoint[2]};
-        Mat color(Size(rsCfg.colorRes[0], rsCfg.colorRes[1]), CV_8UC3, (void*)other_frame.get_data(), Mat::AUTO_STEP);
-
-        if (displayUi(left, up, color,  vec)) {
+        if (displayUi(trackPos[0], trackPos[1] , color,  vec)) {
             break;
         }
     }
-
     return 0;
 }
